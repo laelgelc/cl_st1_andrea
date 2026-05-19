8dc987eea7de7fdb5564dfe1d3d74d2abcf55921
Rogério Yamada <eyamrog@gmail.com>
2026-05-18 21:56:16 -0300

    gitall_20260518-2156

--- cl_st1_ph1_andrea/cl_st1_ph1_andrea.sh
+++ cl_st1_ph1_andrea/cl_st1_ph1_andrea.sh
@@ -1,4 +1,6 @@
 # By default, the script runs in test mode and attempts only the first 5 planned videos
+python download_videos.py
+
 python download_videos.py --cookies env/cookies.txt
 
 #Note: is needed when YouTube blocks automated downloads with authentication or bot-confirmation checks;
@@ -6,6 +8,8 @@ python download_videos.py --cookies env/cookies.txt
 # using a cookies export extension.
 
 # Full run
+python download_videos.py --no-test-mode
+
 python download_videos.py --no-test-mode --cookies env/cookies.txt
 
 # Full run on an EC2 instance

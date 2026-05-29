1cf670e8ded0163e17176511546a7af48d676438
Rogério Yamada <eyamrog@gmail.com>
2026-05-29 19:51:54 -0300

    gitall_20260529-1951

--- cl_st1_ph2_andrea/cl_st1_ph2_andrea.sh
+++ cl_st1_ph2_andrea/cl_st1_ph2_andrea.sh
@@ -6,3 +6,8 @@ python keylemmas.py \
     --output corpus/08_keylemmas \
     --cutoff 3
 # Output: corpus/08_keylemmas/<Decade>.tsv
+
+python select_kws_stratified.py \
+    --per-decade 250 \
+    --max-total 1200
+# Output: corpus/09_kw_selected

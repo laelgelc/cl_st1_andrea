cd27ac3b952e554bddf3978a19b96ef3ef796b22
Rogério Yamada <eyamrog@gmail.com>
2026-05-29 19:30:57 -0300

    gitall_20260529-1930

--- cl_st1_ph2_andrea/cl_st1_ph2_andrea.sh
+++ cl_st1_ph2_andrea/cl_st1_ph2_andrea.sh
@@ -1,7 +1,8 @@
 python tag.py
-# Output: corpus/07_tagged
+# Output: corpus/07_tagged/<Decade>/
 
 python keylemmas.py \
     --input corpus/07_tagged \
     --output corpus/08_keylemmas \
     --cutoff 3
+# Output: corpus/08_keylemmas/<Decade>.tsv

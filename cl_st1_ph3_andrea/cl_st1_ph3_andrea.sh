46eb936e54caae731d0d3350ea9023f6ba21299a
Rogério Yamada <eyamrog@gmail.com>
2026-05-31 08:33:02 -0300

    gitall_20260531-0833

--- cl_st1_ph3_andrea/cl_st1_ph3_andrea.sh
+++ cl_st1_ph3_andrea/cl_st1_ph3_andrea.sh
@@ -70,3 +70,10 @@ python latex_boxplots.py
 # Output: latex_boxplots/slides
 cd ..
 
+python latex_anova_table.py
+
+python latex_anova_tables.py \
+  --project cl_st1_ph3_andrea \
+  --input-dir sas/output_cl_st1_ph3_andrea \
+  --output-dir latex_tables
+# Output: latex_tables
